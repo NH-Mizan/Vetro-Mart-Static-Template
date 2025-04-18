a37e91587dc74d09bec8aa9e86290aa2d23abe49
nh-mizan <nhmizan999@gmail.com>
2025-04-19 01:34:24 +0600

    Banner Section added JQuerry Slider Use and Modifing

---
+++ Js/Script.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function () {
+    $('.banner-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: true,
+      dots: true,
+      infinite: true,
+      speed: 1200,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
